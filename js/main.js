eed38fdb3662fb2772f7ac54b81fcccca37deb41
Alex Story <astory11@yahoo.com>
2014-10-23 14:37:57 -0500

    add geolocation and styling

--- js/main.js
+++ js/main.js
@@ -1,23 +1,41 @@
 var weather;
 var $ul = document.querySelector('.days');
 var $place = 37217;
-var url = 'http://api.wunderground.com/api/79f88a3ca5e3e1e8/forecast10day/q/' + $place + '.json';
-var baseurl = 'http://api.wunderground.com/api/79f88a3ca5e3e1e8/forecast10day/q/';
+var url = 'http://api.wunderground.com/api/79f88a3ca5e3e1e8/geolookup/forecast10day/q/' + $place + '.json';
+var baseurl = 'http://api.wunderground.com/api/79f88a3ca5e3e1e8/geolookup/forecast10day/q/';
 var $button = document.querySelector('.send');
+var $geo = document.querySelector('.geo');
+var $city = document.querySelector('.city')
+var latitude;
+var longitude;
 
 function awesome(data){
   weather = data;
+  $city.innerHTML = weather.location.city;
   for(var i=0 ; i < 5 ; i++){
+    var $img = document.createElement('img')
+    $img.src = weather.forecast.simpleforecast.forecastday[i].icon_url;
     var $li = document.createElement('li');
-    $li.innerHTML = weather.forecast.simpleforecast.forecastday[i].date.weekday + " high: " + weather.forecast.simpleforecast.forecastday[i].high.fahrenheit + " low: " + weather.forecast.simpleforecast.forecastday[i].low.fahrenheit;
+    $li.innerHTML = weather.forecast.simpleforecast.forecastday[i].date.weekday + " high: " + weather.forecast.simpleforecast.forecastday[i].high.fahrenheit + "&#176; low: " + weather.forecast.simpleforecast.forecastday[i].low.fahrenheit + "&#176; ";
+    $li.appendChild($img);
     $ul.appendChild($li);
   }
 }
+
 
 function getJSONP(url, cbString){
   var $script = document.createElement('script');
   $script.src = url + '?callback=' + cbString;
   document.body.appendChild($script);
+}
+
+function show_map(position){
+   latitude = position.coords.latitude;
+   longitude = position.coords.longitude;
+   $place = latitude + ',' + longitude;
+   $ul.innerHTML="";
+   var newurl = baseurl + $place + '.json';
+   getJSONP(newurl, 'awesome');
 }
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -27,5 +45,9 @@ document.addEventListener('DOMContentLoaded', function(){
     $ul.innerHTML="";
     var newurl = baseurl + $place + '.json';
     getJSONP(newurl, 'awesome');
+  });
+  $geo.addEventListener('click', function(){
+    navigator.geolocation.getCurrentPosition(show_map);
+
   });
 });
