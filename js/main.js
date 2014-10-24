f30ebdf08221972d9cc6681b0cc849413f69286a
Alex Story <astory11@yahoo.com>
2014-10-24 15:42:28 -0500

    fixed zip code field

--- js/main.js
+++ js/main.js
@@ -6,22 +6,23 @@ var baseurl = 'http://api.wunderground.com/api/79f88a3ca5e3e1e8/geolookup/foreca
 var $button = document.querySelector('.send');
 var $geo = document.querySelector('.geo');
 var $city = document.querySelector('.city')
-var latitude;
-var longitude;
 
 function awesome(data){
   weather = data;
-  $city.innerHTML = weather.location.city;
-  for(var i=0 ; i < 5 ; i++){
-    var $img = document.createElement('img')
-    $img.src = weather.forecast.simpleforecast.forecastday[i].icon_url;
-    var $li = document.createElement('li');
-    $li.innerHTML = weather.forecast.simpleforecast.forecastday[i].date.weekday + " high: " + weather.forecast.simpleforecast.forecastday[i].high.fahrenheit + "&#176; low: " + weather.forecast.simpleforecast.forecastday[i].low.fahrenheit + "&#176; ";
-    $li.appendChild($img);
-    $ul.appendChild($li);
+  if(weather.response.error){
+    alert(weather.response.error.description);
+  }else{
+    $city.innerHTML = weather.location.city;
+    for(var i=0 ; i < 5 ; i++){
+      var $img = document.createElement('img')
+      $img.src = weather.forecast.simpleforecast.forecastday[i].icon_url;
+      var $li = document.createElement('li');
+      $li.innerHTML = weather.forecast.simpleforecast.forecastday[i].date.weekday + " high: " + weather.forecast.simpleforecast.forecastday[i].high.fahrenheit + "&#176; low: " + weather.forecast.simpleforecast.forecastday[i].low.fahrenheit + "&#176; ";
+      $li.appendChild($img);
+      $ul.appendChild($li);
+    }
   }
 }
-
 
 function getJSONP(url, cbString){
   var $script = document.createElement('script');
@@ -30,8 +31,8 @@ function getJSONP(url, cbString){
 }
 
 function show_map(position){
-   latitude = position.coords.latitude;
-   longitude = position.coords.longitude;
+   var latitude = position.coords.latitude;
+   var longitude = position.coords.longitude;
    $place = latitude + ',' + longitude;
    $ul.innerHTML="";
    var newurl = baseurl + $place + '.json';
@@ -41,17 +42,17 @@ function show_map(position){
 document.addEventListener('DOMContentLoaded', function(){
   getJSONP(url, 'awesome');
   $button.addEventListener('click', function(){
-    $place = document.querySelector('.place').value;
-    if($place.length != 5){
-        alert('please enter a valid 5 digit zip code');
-    } else{
+    $place = Number(document.querySelector('.place').value);
+    if(document.querySelector('.place').value.length === 5 &&  isNaN($place) === false){
+      document.querySelector('.place').value = "";
       $ul.innerHTML="";
       var newurl = baseurl + $place + '.json';
       getJSONP(newurl, 'awesome');
+    } else{
+        alert('please enter a valid 5 digit zip code');
     }
   });
   $geo.addEventListener('click', function(){
     navigator.geolocation.getCurrentPosition(show_map);
-
   });
 });
